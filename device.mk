matt0301/android_device_oneplus_hotdogb

hotdog: Adapt to StatiXOS
* With this first commit I am simply adapting the LineageOS hotdog (OP7T Pro) tree to StatiXOS.

* The commits that follow this one are the ones which will be responsible for the adaptation of the tree to hotdogb (OP7T).

* Nuking the extract blobs scripts too. In their current form I wouldn't fully make use of them.

Change-Id: Ide48b715ee81da402bb945e74dda12ecd926e467

device.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2018-2019 The LineageOS Project
+# Copyright (C) 2020 The StatiXOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -12,11 +13,7 @@ $(call inherit-product, vendor/oneplus/hotdog/hotdog-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
-
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/frameworks/base/packages/SystemUI \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+    $(LOCAL_PATH)/overlay-statix
 
 # A/B
 AB_OTA_PARTITIONS += \

StatiXOS/android_device_oneplus_hotdog

device.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2018-2019 The LineageOS Project
+# Copyright (C) 2020 The StatiXOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -12,10 +13,7 @@ $(call inherit-product, vendor/oneplus/hotdog/hotdog-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
-
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+    $(LOCAL_PATH)/overlay-custom
 
 # A/B
 AB_OTA_PARTITIONS += \